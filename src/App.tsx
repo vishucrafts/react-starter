f28fce4225c4da6d3f12c375732b44fbf598fcf5
vishucrafts <contact@vishu.io>
2023-12-21 15:28:02 +0530

    Too many rerenders

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import SelectMenu from "./components/SelectMenu/SelectMenu";
 import "./App.css";
-import { useState } from "react";
-import Todos from "./components/Todos/Todos";
+import InfiniteRenders from "./components/InfiniteRenders";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	console.log("App renders");
 
 	return (
 		<div>
-			<div>
-				<button onClick={() => setCount(count + 1)}>Increment</button>
-				<p>{count}</p>
-			</div>
-			<div className="wrapper">
-				<SelectMenu
-					label="Select Country"
-					options={["United States", "Canada", "India"]}
-				/>
-				<SelectMenu
-					label="Select Gender"
-					options={["Male", "Female", "Other"]}
-				/>
-			</div>
-			<Todos />
+			<InfiniteRenders />
 		</div>
 	);
 }
