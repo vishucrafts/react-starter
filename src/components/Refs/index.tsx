7d8c3165d587d0019cbda7d4d11d63adfac18541
vishucrafts <contact@vishu.io>
2023-12-21 16:04:17 +0530

    08 Refs

--- src/components/Refs/index.tsx
+++ src/components/Refs/index.tsx
@@ -1,22 +1,24 @@
-import { useEffect, useState } from "react";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { useState } from "react";
 
 export default function Refs() {
 	const [count, setCount] = useState(0);
-	let isFirstRender = true;
+
+	const [isFirstRender] = useState({ current: true });
+
+	console.log("isFirstRender: ", isFirstRender.current);
+
+	//@ts-expect-error
+	isFirstRender.current = false;
 
 	function increment() {
 		setCount(count + 1);
-		// if setCount(0) there is no change so React skip rerender after applying changes from update queue
 	}
 
 	function decrement() {
 		setCount(count - 1);
 	}
 
-	console.log("Rendering Ref component");
-	console.log({ isFirstRender });
-
-	isFirstRender = false;
 	return (
 		<div>
 			<h2>Refs</h2>
@@ -28,61 +30,3 @@ export default function Refs() {
 		</div>
 	);
 }
-
-// export default function Refs() {
-// 	const [count, setCount] = useState(0);
-//   const [isFirstRender, setIsFirstRender] = useState(true);
-
-// 	function increment() {
-// 		setCount(count + 1);
-// 	}
-
-// 	function decrement() {
-// 		setCount(count - 1);
-// 	}
-
-// 	console.log({ isFirstRender });
-
-//   setIsFirstRender(false);
-
-// 	return (
-// 		<div>
-// 			<h2>Refs</h2>
-// 			<div>
-// 				Count: {count}
-// 				<button onClick={increment}>-</button>
-// 				<button onClick={decrement}>+</button>
-// 			</div>
-// 		</div>
-// 	);
-// }
-
-// export default function Refs() {
-// 	const [count, setCount] = useState(0);
-// 	const [isFirstRender, setIsFirstRender] = useState(true);
-
-// 	useEffect(() => {
-// 		setIsFirstRender(false);
-// 	}, []);
-
-// 	function increment() {
-// 		setCount(count + 1);
-// 	}
-
-// 	function decrement() {
-// 		setCount(count - 1);
-// 	}
-
-// 	console.log({ isFirstRender });
-
-// 	return (
-// 		<div>
-// 			<h2>Refs</h2>
-// 			<div>
-// 				Count: {count}
-// 				<button onClick={increment}>-</button>
-// 				<button onClick={decrement}>+</button>
-// 			</div>
-// 		</div>
-// 	);
-// }
