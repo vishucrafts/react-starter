aa7432a038bdcbf19a1ae94ce9733ab3c03511b9
vishucrafts <contact@vishu.io>
2023-12-21 16:14:07 +0530

    08 Refs

--- src/components/Refs/index.tsx
+++ src/components/Refs/index.tsx
@@ -1,15 +1,18 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Refs() {
+	console.log("Refs renders");
+
 	const [count, setCount] = useState(0);
 
-	const [isFirstRender] = useState({ current: true });
+	// let isFirstRender = true; this will recreate the variable on every render
+	const isFirstRender = useRef(true);
 
 	console.log("isFirstRender: ", isFirstRender.current);
 
-	//@ts-expect-error
-	isFirstRender.current = false;
+	useEffect(() => {
+		isFirstRender.current = false;
+	}, []);
 
 	function increment() {
 		setCount(count + 1);
@@ -19,6 +22,10 @@ export default function Refs() {
 		setCount(count - 1);
 	}
 
+	function updateRef() {
+		isFirstRender.current = true;
+	}
+
 	return (
 		<div>
 			<h2>Refs</h2>
@@ -26,7 +33,14 @@ export default function Refs() {
 				Count: {count}
 				<button onClick={increment}>-</button>
 				<button onClick={decrement}>+</button>
+				<button onClick={updateRef}>Update ref</button>
 			</div>
 		</div>
 	);
 }
+
+// function useRef(initialValue: any) {
+// 	const [value] = useState({ current: initialValue });
+
+// 	return value;
+// }
